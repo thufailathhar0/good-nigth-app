5ab5d67361f3c504e8d3f6109e129d356573e48f
Thufail Athhar Baihaqi <thufailathhar0@gmail.com>
2024-11-23 02:06:15 +0700

    Add current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  before_action :authorized!
+
+  def authorized!
+    render json: { errors: "Please add user id" }, status: :unauthorized if current_user.nil?
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: id)
+  end
+
+  def id
+    request.headers["Authorization"]&.delete_prefix("Bearer ")
+  end
 end
