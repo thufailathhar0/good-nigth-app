f37858be37a04a6aa4bb48058ec720663c72ffc1
Thufail Athhar Baihaqi <thufailathhar0@gmail.com>
2024-11-23 09:53:36 +0700

    Improve query for followed sleep records

--- app/controllers/follows/sleeps_controller.rb
+++ app/controllers/follows/sleeps_controller.rb
@@ -4,9 +4,9 @@ class Follows::SleepsController < ApplicationController
   def index
     start_date = 1.week.ago.beginning_of_day
     end_date = Time.current.end_of_day
-    records = Sleep.joins(:user)
-                         .where(user: current_user.followed, clock_in: start_date..end_date)
-                         .order(Arel.sql("(clock_out - clock_in) DESC"))
+    records =  Sleep.includes(:user)
+                    .where(user: current_user.followed, clock_in: start_date..end_date)
+                    .order(Arel.sql("(clock_out - clock_in) DESC"))
     render json: records, each_serializer: FollowedSleepSerializer
   end
 end
